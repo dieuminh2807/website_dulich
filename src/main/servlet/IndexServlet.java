20bd83adc97abd4cbce8937817e612cf5bf34cf8
Dieu   Minh <vothidieuminh2807@gmail.com>
2018-10-18 16:28:28 +0700

    18/10

--- src/main/servlet/IndexServlet.java
+++ src/main/servlet/IndexServlet.java
@@ -13,17 +13,12 @@ public class IndexServlet extends BaseServlet {
     @Override
     public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        List<ChiTietTour>  danhSach1 = TourDB.getTourListByMaLoai("moi");
+        List<ChiTietTour> danhSach1 = TourDB.getTourListByMaLoai("moi");
         List<ChiTietTour> danhSach2 = TourDB.getTourListByMaLoai("nhieunguoidi");
         List<ChiTietTour> danhsach3 = TourDB.getTourListByMaLoai("cokhuyenmai");
         req.setAttribute("tourlist1",danhSach1);
         req.setAttribute("tourlist2",danhSach2);
         req.setAttribute("tourlist3",danhsach3);
-
-
-
-        Map<String,String> listLoaiTour = TourDB.getListMaLoaiTour();
-        req.setAttribute("listLoaiTour", listLoaiTour);
 
         req.getRequestDispatcher("WEB-INF/index.jsp").forward(req, resp);
 
